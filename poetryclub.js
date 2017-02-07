f04a2134fcb4284282d75de9495478e301f07639
ehspoetryclub <ehspoetry@gmail.com>
2017-02-06 19:57:03 -0600

    Update poetryclub.js

--- poetryclub.js
+++ poetryclub.js
@@ -4,7 +4,7 @@ var load = function(){
 
 var more = function() {
 	$("#more").click(function (){
-		$("#moreinfo").slideToggle(680, "swing");
+		$("#moreinfo").slideToggle(500, "swing");
 	});
 };
 
